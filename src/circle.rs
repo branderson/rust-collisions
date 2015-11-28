1e7b1a1ea1a62386dd81610105106c87189343c0
Brad Anderson <brad.anderson1995@gmail.com>
2015-11-27 23:43:07 -0600

    Changes to Circle.rs

--- src/circle.rs
+++ src/circle.rs
@@ -35,11 +35,11 @@ impl Circle {
     }
 
     pub fn get_position(&self) -> (f32, f32) {
-        return (self.x, self.y)
+        (self.x, self.y)
     }
 
     pub fn get_radius(&self) -> f32 {
-        return self.r;
+        self.r
     }
 
     /// Checks if a circle collides with the circle. Returns true if circles collide.
@@ -50,7 +50,7 @@ impl Circle {
         // Make sure neither circle has a negative radius
         assert!(self.r >= 0.0 && other.r >= 0.0);
 
-        // Find all relavant values and cast to f64
+        // Find all relevant values and cast to f64
         let rad_difference: f64 = (self.r - other.r) as f64;
         let rad_sum: f64 = (self.r + other.r) as f64;
         let x_difference: f64 = (self.x - other.x) as f64;
@@ -62,7 +62,7 @@ impl Circle {
         let distance: f64 = x_difference.powi(2) + y_difference.powi(2);
 
         // Return whether distance is within colliding range
-        return distance >= min_distance && distance <= max_distance;
+        distance >= min_distance && distance <= max_distance
     }
 }
 
@@ -112,15 +112,9 @@ mod test {
         assert_eq!(circle1.check_collision(&circle2), true);
         assert_eq!(circle2.check_collision(&circle1), true);
         circle2.set_position(20.1, 0.0);
-        // circle2.x = 20.1;
-        // circle2.y = 0.0;
         assert_eq!(circle1.check_collision(&circle2), false);
         circle1.set_position(0.0, -20.1);
         circle2.set_position(0.0, 0.0);
-        // circle1.x = 0.0;
-        // circle1.y = -20.1;
-        // circle2.x = 0.0;
-        // circle2.y = 0.0;
         assert_eq!(circle1.check_collision(&circle2), false);
     }
 }
